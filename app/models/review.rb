fdaa5bc188d8c6e7d3f1bccaa101353d748563a6
Joao Ramires <joaoramires.engsoft@gmail.com>
2020-04-21 20:49:15 -0300

    Chore: Populate Data Base

---
+++ app/models/review.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+
+  validates :content, length: { minimum: 5 }
+
+  validates :rating,
+            numericality: true,
+            inclusion: {
+              in: 0..5
+            }
+end
